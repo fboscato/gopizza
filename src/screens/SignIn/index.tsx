7b8a4134b7960bb14fc2ca977f5b0b576809fe17
fboscato <fboscato@gmail.com>
2022-11-23 17:46:16 -0300

    colocado inputs

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import { Container } from "./style";
-export function SignIn(){
-    return(
-<Container>
-    
-</Container>
+import { Input } from "@components/Input";
+export function SignIn() {
+    return (
+        <Container>
+            <Input
+                placeholder="E-mail"
+                type="secondary"
+                autoCorrect={false}
+                autoCapitalize="none"
+            />
+            <Input
+                placeholder="Senha"
+                type="secondary"
+                secureTextEntry
+
+            />
+        </Container>
     )
 }

d4cb848e820aacaa3385c974c02abed795deb6ea
fboscato <fboscato@gmail.com>
2022-11-25 17:50:34 -0300

    image

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,26 +1,34 @@
 import React from "react";
-import { Container } from "./style";
+import { Container, Content, Tile, Brand } from "./style";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import branfImg from '@assets/brand.png'
+import { KeyboardAvoidingView, Platform } from 'react-native'
 export function SignIn() {
     return (
         <Container>
-            <Input
-                placeholder="E-mail"
-                type="secondary"
-                autoCorrect={false}
-                autoCapitalize="none"
-            />
-            <Input
-                placeholder="Senha"
-                type="secondary"
-                secureTextEntry
+            <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined} >
+                <Content>
+                    <Brand source={branfImg} />
+                    <Tile>Login</Tile>
+                    <Input
+                        placeholder="E-mail"
+                        type="secondary"
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                    />
+                    <Input
+                        placeholder="Senha"
+                        type="secondary"
+                        secureTextEntry
 
-            />
-            <Button
-            title="Entrar"
-            type="secundary"
-            />
-        </Container>
+                    />
+                    <Button
+                        title="Entrar"
+                        type="secundary"
+                    />
+                </Content>
+            </KeyboardAvoidingView>
+        </Container >
     )
 }
--- src/screens/SignIn/style.ts
+++ src/screens/SignIn/style.ts
@@ -1,10 +1,41 @@
-import styled from 'styled-components/native'
-import {LinearGradient} from 'expo-linear-gradient'
-export const Container = styled(LinearGradient).attrs(({theme})=>({
-    colors: theme.COLORS.GRADIENT,
-    start: {x:0, y:1},
-    end: {x:0.5, y:0.5}
+import styled, { css } from 'styled-components/native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+
+export const Container = styled(LinearGradient).attrs(({ theme }) => ({
+  colors: theme.COLORS.GRADIENT,
+  start: { x: 0, y: 1 },
+  end: { x: 0.5, y: 0.5 },
 }))`
-flex: 1;
-justify-content: center;
-`
+  flex: 1;
+  justify-content: center;
+`;
+
+export const Content = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    padding: getBottomSpace() + 48,
+  },
+})`
+  width: 100%;
+  padding: 0px 32px;
+`;
+
+export const Tile = styled.Text`
+  font-size: 32px;
+  margin-bottom: 24px;
+  align-self: flex-start;
+
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.TITLE};
+    color: ${theme.COLORS.TITLE};
+  `};
+`;
+
+export const Brand = styled.Image.attrs({
+  resizeMode: 'contain',
+})`
+  height: 340px;
+  margin-top: 64px;
+  margin-bottom: 32px;
+`;
